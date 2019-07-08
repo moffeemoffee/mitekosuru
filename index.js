6adb3039f2c8d25928d195d06cc40eb8b96836c2
moffeemoffee <festbawi@gmail.com>
2019-07-08 13:43:05 +0800

    Changed version

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
+const pjson = require('./package.json')
 const program = require('commander')
 
 program
-  .version('0.0.1')
+  .version(pjson.version)
   .option('-d --debug', 'output extra debugging')
-
-program.parse(process.argv)
+  .parse(process.argv)
 
 module.exports = () => {}
