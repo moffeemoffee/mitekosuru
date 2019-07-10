56447892fc2ae746c4e873ea18b55d2f32aad7c9
moffeemoffee <festbawi@gmail.com>
2019-07-10 20:29:44 +0800

    Added some incomplete pkg. Attempted to fix cmoaJp stitching

--- utils/logger.js
+++ utils/logger.js
@@ -2,15 +2,15 @@ const chalk = require('chalk')
 const { createLogger, format, transports } = require('winston')
 
 module.exports = createLogger({
-  level: 'debug',
+  level: 'info',
   format: format.combine(
     format.colorize(),
     format.timestamp({
-      format: 'YYYY-MM-DD HH:mm:ss'
+      format: 'YYYY-MM-DD HH:mm:ss',
     }),
     format.printf(
-      info => `[${chalk.magenta('Mitekosuru')}] ${info.timestamp} ${info.level}: ${info.message}`
+      (info) => `[${chalk.magenta('Mitekosuru')}] ${info.timestamp} ${info.level}: ${info.message}`
     )
   ),
-  transports: [new transports.Console({ level: 'debug' })]
+  transports: [new transports.Console({ level: 'info' })],
 })
